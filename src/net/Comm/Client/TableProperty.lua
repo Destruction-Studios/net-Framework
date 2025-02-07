ee26a6f6090abc20142aaf01e87063e986273553
Destruction Studios <founderofbeans@gmail.com>
2025-02-07 12:06:02 -0500

    Added Server Table Property

---
+++ src/net/Comm/Client/TableProperty.lua
@@ -0,0 +1,10 @@
+
+local TableProperty = {}
+local TablePropertyMT = {}
+TablePropertyMT.__index = TablePropertyMT
+
+function TableProperty.new()
+    
+end
+
+return TableProperty
--- src/net/Comm/Server/init.lua
+++ src/net/Comm/Server/init.lua
@@ -1,5 +1,6 @@
 return {
     RemoteEvent = require(script.RemoteEvent),
     Property = require(script.Property),
+    TableProperty = require(script.TableProperty),
     Function = require(script.Function),
 }
